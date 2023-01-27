3fdbb4ed045d0705fb616080f0b0c200b0c942b1
Abel-orion <abelorion16@gmail.com>
2023-01-27 12:48:18 -0800

    update

--- write_handlers.c
+++ write_handlers.c
@@ -126,7 +126,7 @@ int write_num(int ind, char buffer[],
 		{
 			if (extra_c)
 				buffer[--padd_start] = extra_c;
-			return (write(1, &buffer[padd_start], i - padd_start)
+			return (write(1, &buffer[padd_start], i - padd_start)							+
 
 					write(1, &buffer[ind], length - (1 - padd_start)));
 		}
